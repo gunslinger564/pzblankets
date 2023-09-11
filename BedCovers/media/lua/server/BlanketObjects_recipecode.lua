b4804b7fabf70e2d6f50fb1e8beecc9bfa88782f
gunslinger564 <144602357+gunslinger564@users.noreply.github.com>
2023-09-11 16:22:21 -0500

    Update BlanketObjects_recipecode.lua

--- BedCovers/media/lua/server/BlanketObjects_recipecode.lua
+++ BedCovers/media/lua/server/BlanketObjects_recipecode.lua
@@ -3,3 +3,11 @@ require "recipecode"
 function Recipe.OnGiveXP.Tailoring3(recipe, ingredients, result, player)
     player:getXp():AddXP(Perks.Tailoring, 3)
 end
+
+function RipBlanket.Oncreate(item,result,player)
+     local nbr = ZombRand(2,6)
+        nbr = nbr + (player:getPerkLevel(Perks.Tailoring) / 2)
+        for i, nbr do
+        player:getInventory():AddItem("Base.RippedSheets")
+        end
+end
