a6470b765212211b9dba740f6f4b5554e58fc860
gunslinger564 <144602357+gunslinger564@users.noreply.github.com>
2023-09-23 22:09:11 -0500

    Update BlanketObjects_PlaceBlanket.lua

--- BedCovers/media/lua/client/BlanketObjects_PlaceBlanket.lua
+++ BedCovers/media/lua/client/BlanketObjects_PlaceBlanket.lua
@@ -10,19 +10,25 @@ function BO.placeBedSheet(character,bed,bedSheet,tileset)
 			local obj = objects:get(i)
 
 			obj:setSprite(getSprite((obj:getTextureName():gsub("furniture_bedding_01",tileset,1))))
+			if tileset[2] ~= nil then
+				local patternSheet = tileset[2] .. "_" .. string.match(obj:getTextureName(),"%d+$")
+				obj:setOverlaySprite(patternSheet)
+				obj:getModData().patternOverlay = tileset[2]
+				obj:transmitModData()
+			end
 			obj:transmitUpdatedSpriteToServer()
 		end
 	end
 end
 
 function BO.removeBedSheet(character,bed,tileset,item)
-	if bed:getTextureName():find(tileset) ~= nil then return end
+	if bed:getTextureName():find(tileset[1]) ~= nil then return end
 
 	local objects = ArrayList.new()
 	bed:getSpriteGridObjects(objects)
 	for i = 0, objects:size()- 1 do
 		local obj = objects:get(i)
-        obj:setSprite(getSprite((obj:getTextureName():gsub(tileset,"furniture_bedding_01",1))))
+        obj:setSprite(getSprite((obj:getTextureName():gsub(tileset[1],"furniture_bedding_01",1))))
 		obj:transmitUpdatedSpriteToServer()
 	end
 	character:getInventory():AddItem(item)
@@ -46,21 +52,22 @@ function BO.OnPreFillWorldObjectContextMenu(player, context, worldobjects, test)
         local bed_tileset = bed:getTextureName():gsub("_%d+$","")
 		if bed_tileset == "furniture_bedding_01" and not BO.IgnoreTileList[bed:getTextureName()] then
             local inventory = character:getInventory()
-            for item,tileset in pairs(BO.TilesInfo) do
-                if inventory:containsType(item) then
-                    local optionBed = context:addOption(getText("ContextMenu_BO_PlaceBedSheet",getItemNameFromFullType(item)),character,BO.placeBedSheet,bed,item,tileset)
-                end
-            end
+				for item,tileset in pairs(BO.TilesInfo) do
+					if inventory:containsType(item) then
+						local optionBed = context:addOption(getText("ContextMenu_BO_PlaceBedSheet",getItemNameFromFullType(item)),character,BO.placeBedSheet,bed,item,tileset)
+					end	
+				end
+			
+		end
 		else
             for item,tileset in pairs(BO.TilesInfo) do
-                if bed_tileset == tileset then
+                if bed_tileset == tileset[1] then 
                     local removeBlanket = context:addOption(getText("ContextMenu_BO_RemoveBedSheet"),character,BO.removeBedSheet,bed,tileset,item)
-                    break
+			break
                 end
             end
-		end
 	end
-
 end
+
 
 Events.OnFillWorldObjectContextMenu.Add(BO.OnPreFillWorldObjectContextMenu)
