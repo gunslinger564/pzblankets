3c8b53b3ae4779bb7731ab438a38c550434e3d68
gunslinger564 <144602357+gunslinger564@users.noreply.github.com>
2025-01-09 04:27:00 -0600

    Update BlanketObjects_PlaceBlanket.lua

--- BedCovers/42/media/lua/client/BlanketObjects_PlaceBlanket.lua
+++ BedCovers/42/media/lua/client/BlanketObjects_PlaceBlanket.lua
@@ -10,18 +10,23 @@ function BO.placeBedSheet(character,bed,bedSheet,tileset)
 	if character:getInventory():RemoveOneOf(bedSheet,false) then
 		print("placeBedSheet removeing inventory item attempt")
 		local objects = ArrayList.new()
-		bed:getSpriteGridObjects(objects)
-		-- if objects:size() < bed:getSprite():getSpriteGrid():getSpriteCount() then return character:Say("broken bed") end --text
-		local patternGrid = BlanketObjects.SpriteUtil.getMatchingSpriteGridForData(bed,bedSheetItem:getModData())
+		bed:getSpriteGridObjectsIncludingSelf(objects)
+
+		--broken code
+		--local patternGrid = BlanketObjects.SpriteUtil.getMatchingSpriteGridForData(bed,bedSheetItem:getModData())
+
+
 		for i = 0, objects:size()- 1 do
 			local obj = objects:get(i)
 			local newName = obj:getTextureName():gsub("furniture_bedding_01",tileset,1)
 			local newSprite = getSprite(newName)
 			obj:addAttachedAnimSprite(newSprite)
-			--obj:setSprite(getSprite((obj:getTextureName():gsub("furniture_bedding_01",tileset,1))))
-			if patternGrid ~= nil then
-				BlanketObjects.SpriteUtil.addPattern(obj,patternGrid:getSpriteFromIndex(i):getName(),bedSheetItem:getModData().movableData.bedcoverData)
-			end
+
+			--broken code
+		--	if patternGrid ~= nil then
+		--		BlanketObjects.SpriteUtil.addPattern(obj,patternGrid:getSpriteFromIndex(i):getName(),bedSheetItem:getModData().movableData.bedcoverData)
+		--	end
+		
 			obj:transmitUpdatedSpriteToServer()
 		end
 	end
@@ -32,19 +37,18 @@ end
 ---@param tileset string
 ---@param item string
 function BO.removeBedSheet(character,bed,tileset,item)
-	if bed:getTextureName():find(tileset) == nil then return end
-	local itemObj = character:getInventory():AddItem(item)
+	--if bed:getTextureName():find(tileset) == nil then return end
+	local itemObj = instanceItem(item)
+	character:getInventory():addItem(itemObj)
 	if itemObj ~= nil then
 		if not itemObj:getModData().movableData then itemObj:getModData().movableData = {} end
 		itemObj:getModData().movableData.bedcoverData = (bed:getModData().movableData or {}).bedcoverData
 	end
 	local objects = ArrayList.new()
-	bed:getSpriteGridObjects(objects)
+	bed:getSpriteGridObjectsIncludingSelf(objects)
 	for i = 0, objects:size()- 1 do
 		local obj = objects:get(i)
 		obj:clearAttachedAnimSprite()
-		--obj:setSprite(getSprite((obj:getTextureName():gsub(tileset,"furniture_bedding_01",1))))
-		BlanketObjects.SpriteUtil.removePattern(obj)
 		obj:transmitUpdatedSpriteToServer()
 	end
 end
@@ -65,22 +69,32 @@ function BO.OnPreFillWorldObjectContextMenu(player, context, worldobjects, test)
 	if bed ~= nil then
 		local character = getSpecificPlayer(player)
 		local bed_tileset = bed:getTextureName():match("(.+)_%d+$")
-		if bed_tileset == "furniture_bedding_01" and bed:getProperties():Is("BedCoverType") then
+		print(bed_tileset)
+		print("Attached Sprite Count:  "..bed:getAttachedAnimSpriteCount())
+		if bed_tileset == "furniture_bedding_01" and bed:getProperties():Is("BedCoverType") and bed:getAttachedAnimSpriteCount() == 0 then
 			local inventory = character:getInventory()
 			for item,tileset in pairs(BO.TilesInfo) do
 				if inventory:containsType(item) then
 					local optionBed = context:addOption(getText("ContextMenu_BO_PlaceBedSheet",getItemNameFromFullType(item)),character,BO.placeBedSheet,bed,item,tileset)
 				end
 			end
-		else
-			for item,tileset in pairs(BO.TilesInfo) do
-				if bed_tileset == tileset then
-					local removeBlanket = context:addOption(getText("ContextMenu_BO_RemoveBedSheet"),character,BO.removeBedSheet,bed,tileset,item)
-					break
+		elseif bed_tileset == "furniture_bedding_01" and bed:getProperties():Is("BedCoverType") and bed:getAttachedAnimSpriteCount() >0  then
+			local sprites = bed:getAttachedAnimSprite()
+			local item
+			local tileset
+			for pickItem,pickTileset in pairs(BO.TilesInfo)do
+				for i = 0, sprites:size()-1 do
+					local spriteName = sprites:get(i):getName()
+					spriteName = spriteName:gsub(spriteName:sub(-2),"")
+					if string.find(spriteName,pickTileset) then
+						item = pickItem
+					end
 				end
 			end
+			local removeBlanket = context:addOption(getText("ContextMenu_BO_RemoveBedSheet"),character,BO.removeBedSheet,bed,nil,item)
 		end
 	end
 end
 
 Events.OnFillWorldObjectContextMenu.Add(BO.OnPreFillWorldObjectContextMenu)
+
